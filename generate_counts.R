e5edba7aa353457da0e2b5bfddcf77e39ccc0a83
Martino Colombo <martino.colombo8@gmail.com>
2016-12-06 18:02:07 +0100

    batch now with random strenght and width effects

--- generate_counts.R
+++ generate_counts.R
@@ -1,38 +1,65 @@
+library(compcodeR)
+library(methods)
+
 ### set options ### ### ###
 
-nGenes=10000
-reps=3
-filterTS=0
-eSize=1.5
-bgdd=F
-nDiff=1000
-depth=1e+07
+n_genes = 10000
+batch_strength_min = 0
+batch_strength_max = 2
+batch_width_min = 0
+batch_width_max = 1
+n_replicates = 3
+min_counts_gene = 0
+n_degs = 1000
+deg_min_fc = 1.5
+depth = 1e+07
 
-batchStInter=seq(0,0.5,by=0.1)
+### batch simulation function ### ### ###
+batch_simulation = function(dat, strength_min=0, strength_max=2, width_min=0, width_max=1, min_counts_gene=0){
+	counts = dat@count.matrix
+	n_replicates = length(counts[1,]) / 2
+	n_genes = length(counts[,1])
+	
+	for (i in seq(n_replicates)){
+		batch_strength = runif(1, strength_min, strength_max)
+		n_batch_genes = round(runif(1, width_min, width_max) * n_genes)
+		batch_genes = sample(seq(n_genes))[1:n_batch_genes]
+		batch_effect = rep(1,n_genes)
+		batch_effect[batch_genes] = abs(rnorm(n_batch_genes, sd=batch_strength)) + 1
+		half_batch_genes = batch_genes[1:round(n_batch_genes / 2)]
+		batch_effect[half_batch_genes] = 1 / batch_effect[half_batch_genes]
+		dat@count.matrix[,i] = round(dat@count.matrix[,i]*batch_effect)
+		dat@count.matrix[,n_replicates+i] = round(dat@count.matrix[,n_replicates+i]*batch_effect)
+	}
+
+	dat@count.matrix = dat@count.matrix[which(rowSums(dat@count.matrix)>=(min_counts_gene)),]
+	return(dat)
+}
+
 
 ### simulate! ### ### ###
 
-source("../old/functions.R")
-library(compcodeR)
-
-for(k in seq(20)){
+for(k in seq(2)){
 
 # generate data
-	dat=generateSyntheticData(dataset="simulated",
-		n.vars=nGenes,
-		samples.per.cond=reps,
+	dat = generateSyntheticData(dataset="simulated",
+		n.vars=n_genes,
+		samples.per.cond=n_replicates,
 		seqdepth=depth,
-		n.diffexp=nDiff,
+		n.diffexp=n_degs,
 		fraction.upregulated=0.5,
-		between.group.diffdisp=bgdd,
-		effect.size=eSize,
+		between.group.diffdisp=F,
+		effect.size=deg_min_fc,
 		repl.id=1,
-		filter.threshold.total=filterTS)
-	write.table(dat@count.matrix,file=paste0("counts",k),col.names=FALSE,quote=FALSE)
+		filter.threshold.total=min_counts_gene)
+	write.table(dat@count.matrix, file=paste0("counts",k), col.names=FALSE, quote=FALSE)
 
 # simulate batch
-	for(batchSt in batchStInter){
-		datB=batchEffect(dat,batchSt=batchSt,filterTS=filterTS,tripleEff=TRUE)
-		write.table(datB@count.matrix,file=paste0("counts",k,"_",batchSt),col.names=FALSE,quote=FALSE)
-	}
+	dat_batch = batch_simulation(dat,
+		strength_min=batch_strength_min,
+		strength_max=batch_strength_max,
+		width_min=batch_width_min,
+		width_max=batch_width_max,
+		min_counts_gene=min_counts_gene)
+	write.table(dat_batch@count.matrix, file=paste0("counts", k), col.names=FALSE, quote=FALSE)
 }
