22ebceb020d37862ce1db17e765684a00f87039c
Martino Colombo <martino.colombo8@gmail.com>
2016-12-22 14:24:16 +0100

    returns n.sv

--- compute_accuracy.R
+++ compute_accuracy.R
@@ -1,48 +1,61 @@
 library(limma)
 library(edgeR)
+library(DESeq2)
+library(sva)
 
-counts_dir="~/Dropbox/projects/microprojects/covariates/counts/"
-simuls = 2#seq(20)
-batches = seq(0,1,by=0.2)
-group = rep(c("A","B"),each=3)
-group_b = rep(c(1,2,3),2)
+n_simuls = 2
+n_replicates = 3
+group = rep(c("A","B"),each=n_replicates)
+group_b = rep(seq(n_replicates),2)
 design = model.matrix(~group)
-design_b1 = model.matrix(~group+group_b)
 n_DEGs = 1000
 n_top = 1000
 
 
-voom_res=function(counts_file,group,design,n_top){
-		counts=as.matrix(read.table(counts_file,row.names=1))
-		dge=DGEList(counts=counts,group=group)
-		dge=calcNormFactors(dge)
-		v=voom(dge,design)
-		fit=lmFit(v,design)
-		fit=eBayes(fit)
-		return(topTable(fit,n=n_top,coef=2))
+voom_res = function(counts_file, group, design, n_top){
+		counts = as.matrix(read.table(counts_file, row.names=1))
+		dge = DGEList(counts=counts, group=group)
+		dge = calcNormFactors(dge)
+		v = voom(dge, design)
+		fit = lmFit(v, design)
+		fit = eBayes(fit)
+		return(topTable(fit, n=n_top, coef=2))
 }
-aucc=function(res,n_DEGs,n_top){
-	res$gene_n=as.numeric(gsub("g","",row.names(res)))
+
+aucc = function(res, n_DEGs, n_top){
+	res$gene_n = as.numeric(gsub("g", "", row.names(res)))
 	tpr=0; auc=0; fp=0
 	for(n in seq(length(res[,1]))){
-		if(res$gene_n[n]<n_DEGs){
-			tpr=tpr+1/n_DEGs
+		if(res$gene_n[n] < n_DEGs){
+			tpr = tpr + 1 / n_DEGs
 		}else{
 			auc=auc+tpr; fp=fp+1
 		}
 	}
 	auc=auc+tpr; fp=fp+1
-	return(auc/fp)
+	return(auc / fp)
 }
 
-
-for(n_simuls in simuls){
-	for(n_batch in batches){
-		counts_file = paste0(counts_dir,"counts",n_simuls,"_",n_batch) 
-		top_res = voom_res(counts_file,group,design,n_top)
-		auc = aucc(top_res,n_DEGs,n_top)
-		top_res_b = voom_res(counts_file,group,design_b1,n_top)
-		auc_b = aucc(top_res_b,n_DEGs,n_top)
-		print(c(auc,auc_b))
-	}
+sva_corr = function(counts, group){
+	dds = DESeqDataSetFromMatrix(countData=counts, colData=data.frame(group=group), design=~group)
+	dds = estimateSizeFactors(dds)
+	dat = counts(dds, normalized=TRUE)
+	dat = dat[rowMeans(dat)>5,]
+	mod = model.matrix(~group, colData(dds))
+	mod0 = model.matrix(~1, colData(dds))
+	svseq = svaseq(dat, mod, mod0)
+	return(svseq)
 }
+
+for (simul in seq(n_simuls)){
+	counts_file = paste0("counts", simul) 
+	counts = read.table(counts_file, row.names=1)
+	n_sv = sva_corr(counts, group)$n.sv
+	print(n_sv)
+}
+
+#	top_res = voom_res(counts_file, group, design, n_top)
+#	auc = aucc(top_res, n_DEGs, n_top)
+#	top_res_b = voom_res(counts_file, group, design_b1, n_top)
+#	auc_b = aucc(top_res_b, n_DEGs, n_top)
+#	print(c(auc, auc_b))
