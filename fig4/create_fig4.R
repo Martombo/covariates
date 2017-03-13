8f3f4a892f29d63e2a66a3e5036ace0f4137a2e5
Martino Colombo <martino.colombo8@gmail.com>
2017-03-13 18:01:45 +0100

    modified figure 4 and 5 to include the no-covariate case

--- fig4/create_fig4.R
+++ fig4/create_fig4.R
@@ -1,29 +1,29 @@
 library(ggplot2)
-#library(sva)
-#source("../generate_counts.R")
-#
-#set.seed(5432)
-#n_samples = c(seq(3, 12, by=3))
-#n_covars = c(1,2,3)
-#
-#est_covar = data.frame(n_samples=rep(n_samples,length(n_covars)))
-#est_covar$n_covars = rep(n_covars,each=length(n_samples))
-#est_covar$n_est_covars = as.double(rep(n_covars,each=length(n_samples)))
-#est_covar$legend = factor("true",levels=c("true","be","leek"))
-#for (n_sim_covar in n_covars){
-#	for (k in n_samples){
-#		n_replicates = c(k, k)
-#		data = simulate_dataset(n_replicates, 1, n_covariates=n_sim_covar, cov_width=1, cov_strength_max=2)
-#		group = c(rep("A", n_replicates[1]), rep("B", n_replicates[2]))
-#		num_sv_be = num.sv(as.matrix(read.table("counts1")), model.matrix(~group))
-#		num_sv_leek = num.sv(as.matrix(read.table("counts1")), model.matrix(~group), method="leek")
-#		est_covar = rbind(est_covar, c(k, n_sim_covar, num_sv_be, "be"))
-#		est_covar = rbind(est_covar, c(k, n_sim_covar, num_sv_leek, "leek"))
-#	}
-#}
-#
-#save.image()
-load(".RData")
+library(sva)
+source("../generate_counts.R")
+
+set.seed(5432)
+n_samples = c(seq(3, 12, by=3))
+n_covars = c(0,1,2)
+
+est_covar = data.frame(n_samples=rep(n_samples,length(n_covars)))
+est_covar$n_covars = rep(n_covars,each=length(n_samples))
+est_covar$n_est_covars = as.double(rep(n_covars,each=length(n_samples)))
+est_covar$legend = factor("true",levels=c("true","be","leek"))
+for (n_sim_covar in n_covars){
+	for (k in n_samples){
+		n_replicates = c(k, k)
+		data = simulate_dataset(n_replicates, 1, n_covariates=n_sim_covar, cov_width=1, cov_strength_max=2)
+		group = c(rep("A", n_replicates[1]), rep("B", n_replicates[2]))
+		num_sv_be = num.sv(as.matrix(read.table("counts1")), model.matrix(~group))
+		num_sv_leek = num.sv(as.matrix(read.table("counts1")), model.matrix(~group), method="leek")
+		est_covar = rbind(est_covar, c(k, n_sim_covar, num_sv_be, "be"))
+		est_covar = rbind(est_covar, c(k, n_sim_covar, num_sv_leek, "leek"))
+	}
+}
+
+save.image()
+#load(".RData")
 
 est_covar$n_est_covars = as.double(est_covar$n_est_covars)
 est_covar$n_samples = as.double(est_covar$n_samples)
--- fig5/create_fig5.R
+++ fig5/create_fig5.R
@@ -3,17 +3,18 @@ source("../generate_counts.R")
 source("../compute_accuracy.R")
 
 n_simulations = 20
-n_covariates = c(1,2,3)
+n_covariates = c(1,2)
 set.seed(1234)
 
 simulate_dataset(NA, n_simulations, n_covariates=0)
 aucs = compute_all_accuracy(n_simulations, 0)
 for (n_covar in n_covariates){
-	simulate_dataset(NA, n_simulations, n_covariates=n_covar, depth=NA, n_degs=NA)
+	simulate_dataset(NA, n_simulations, n_covariates=n_covar, cov_strength_min=0.2, depth=NA, n_degs=NA)
 	aucs = rbind(aucs, compute_all_accuracy(n_simulations, n_covar))
 }
 save.image()
 
+#checking leek n of covars
 p = ggplot(aucs, aes(x=corr, y=auc, fill=corr))
 svg("fig5.svg")
 	p + geom_boxplot() + facet_wrap(~n_covar, ncol=1)
